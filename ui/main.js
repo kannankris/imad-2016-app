2f601b96c2865d181fedaf88fc50b45a5fb3cb62
kannankris <tmkrishnanunni@gmail.com>
2016-09-25 19:08:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+var button1 = document.getElementById('counter');
+var counter = 0;
+button1.onclick = function () {
+    // make a request to counter
+    
+    //capture the responce to a variable
+    
+    //render the variable to page
+    counter = counter + 1;
+    var spinj = document.getElementById('count');
+    spinj.innerHTML = counter.toString();
+};

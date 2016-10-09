47a0a39714772a73ebba0dbfa650a5bebb81eafd
kannankris <tmkrishnanunni@gmail.com>
2016-10-10 00:52:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,16 +27,33 @@ button1.onclick = function () {
 
 
 //submitting the name
-var inpname = document.getElementById('iname');
-var valname = inpname.value;
+
 var submitbut = document.getElementById('subname');
 submitbut.onclick = function(){
+    // create a request
+    var request = new XMLHttpRequest(); 
+    //capture the responce to a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if (request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i = 0; i < names.length; i++ ) {
+                    list += '<li>' + names[i] +'</li>';
+                }
+                var newlist = document.getElementById('namelist');
+                newlist.innerHTML = list;
+            }
+        }
+    };
+
+
+// making the request
+    var inpname = document.getElementById('iname');
+    var valname = inpname.value;
+    request.open('GET', 'http://kannankris.imad.hasura-app.io/inputname?name=' + valname, true);
+    request.send(null);
+
     
-    var names = ['onnaman', 'randaman', 'moonaman'];
-    var list = '';
-    for (var i = 0; i < names.length; i++ ) {
-        list += '<li>' + names[i] +'</li>';
-    }
-    var newlist = document.getElementById('namelist');
-    newlist.innerHTML = list;
 };
